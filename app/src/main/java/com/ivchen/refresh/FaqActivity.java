4100609b9e9b669d9c7c66983c0d0e38dc52aa5d
Lawrence Chen <the_crimson_Chen@yahoo.com>
2016-04-16 21:06:24 -0400

    Finished backgrounds and added intents

--- app/src/main/java/com/ivchen/refresh/FaqActivity.java
+++ app/src/main/java/com/ivchen/refresh/FaqActivity.java
@@ -1,6 +1,5 @@
 package com.ivchen.refresh;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,18 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ChooseActivity extends AppCompatActivity {
+public class FaqActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose);
-
+        setContentView(R.layout.activity_faq);
     }
 
-    public void onClickFaq(View view) {
-        Intent intent = new Intent(ChooseActivity.this, FaqActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
-    }
 }
